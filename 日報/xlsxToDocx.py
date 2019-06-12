f09735237468ef2310bb057b311058bbf51698dd
高城友豪 <a217048@ns.kogakuin.ac.jp>
2019-06-12 17:19:05 +0900

    nameタグの追加
    誰が書いたのか見やすくする為にnameタグを追加した.

--- 日報/xlsxToDocx.py
+++ 日報/xlsxToDocx.py
@@ -2,6 +2,8 @@ import docx
 import xlrd
 import pprint
 import xlwt
+from docx.shared import RGBColor
+from docx.shared import Pt
 
 sheetName = input("ファイル名(xxx)を入力(xxx.xlsx形式)>>> ")
 doc = docx.Document()
@@ -14,11 +16,21 @@ for i, e in enumerate(row):
     col = sheet.col_values(i)
     cols.append(col)
 
+for i, e in enumerate(row):
     for ii, ee in enumerate(cols[i]):
         # print("{}>>> {}".format(i, cols[i][ii]))
         if ii == 0:
             doc.add_heading(cols[i][ii], 0)
         else:
-            doc.add_paragraph(str(cols[i][ii]))
+            # print(cols[len(cols)-1][ii].color.type)
+            # doc.add_paragraph(str(cols[len(cols)-1][ii]) +":")
+            #####
+            run = doc.add_paragraph().add_run(str(cols[len(cols)-1][ii]) +"  :")
+            font = run.font
+            font.color.rgb = RGBColor(47, 145, 79)
+            font.size = Pt(8)
+            #####
+            doc.add_paragraph("   " +str(cols[i][ii]))
 ################################################
 doc.save('./{}.docx'.format(sheetName))
+print("fin")
