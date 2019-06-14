8499983f8bccf1e131519200dcf417ac9b91157c
高城友豪 <a217048@ns.kogakuin.ac.jp>
2019-06-14 22:35:47 +0900

    名前タグで名前リストを認識
    名前リストを順番で認識するのでは無く,名前タグによって名前リストを認識した.

--- 日報/xlsxToDocx.py
+++ 日報/xlsxToDocx.py
@@ -5,6 +5,19 @@ import xlwt
 from docx.shared import RGBColor
 from docx.shared import Pt
 from tqdm import tqdm
+
+#####__def__####################################################################
+def makingNameList(col):
+    name = [""]
+    # if "名前"==str(col[0]) or "なまえ"==str(col[0]) or "name"== str(col[0]):
+    for ii, ee in enumerate(col):
+        if ii == 0:
+            continue
+        else:
+            name.append(col[ii])
+    # print("makingNameList {}".format(col))
+    return name
+################################################################################
 #####__init__###################################
 sheetList = input("ファイル名(xxx)を入力(xxx.xlsx形式)>>> ")
 sheetNames = sheetList.split(",")
@@ -20,14 +33,22 @@ for sheetName in tqdm(sheetNames):
     # paragraph.size = Pt(16)
     paragraph.text = sheetName
     ################################################
-    row = sheet.row_values(0)
+    row = sheet.row_values(0)#質問内容の保存
     cols = []
+    name = []
     for i, e in enumerate(row):
         col = sheet.col_values(i)
         cols.append(col)
+        # print("col = {}".format(col[0]))
+        #####__making_name__####################################################
+        if "名前"==str(col[0]) or "なまえ"==str(col[0]) or "name"== str(col[0]):
+            name = makingNameList(col)#名前の配列の作成
+        ########################################################################
 
     for i, e in enumerate(row):
-        if i == 0:
+        # if i == 0:
+        #     continue
+        if "名前" == e or "なまえ" == e or "name" == e:
             continue
         else:
             for ii, ee in enumerate(cols[i]):
@@ -41,7 +62,9 @@ for sheetName in tqdm(sheetNames):
                         # print(cols[len(cols)-1][ii].color.type)
                         # doc.add_paragraph(str(cols[len(cols)-1][ii]) +":")
                         #####
-                        run = doc.add_paragraph().add_run(str(cols[len(cols)-1][ii]) +"  :")
+                        # run = doc.add_paragraph().add_run(str(cols[len(cols)-1][ii]) +"  :")
+                        print("name[{}] = {}".format(ii, name[ii]))
+                        run = doc.add_paragraph().add_run(str(name[ii]) +"  :")
                         font = run.font
                         font.color.rgb = RGBColor(47, 145, 79)
                         font.size = Pt(8)
